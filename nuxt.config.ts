d68cb446f86fe69174f9d49eda34e377f8d4458b
Tomaz Xavier <tomazcx06@gmail.com>
2024-07-31 21:55:57 -0300

    feat: initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,23 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss"],
+  tailwindcss: {
+    config: {
+      theme: {
+        extend: {
+          colors: {
+            primary: "#B692E6",
+	    secondary: "#501997",
+	    background: "#13091F",
+	    text: "#E9E1F3",
+	    accent: "#761FE7",
+	    backgroundSecondary: "#251737",
+	    textSecondary: "#ADA9B1"
+          },
+        },
+      },
+    },
+  },
+});
