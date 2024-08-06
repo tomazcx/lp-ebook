2aa10917aa089acb824710040faa52ca34a2d65b
Tomaz Xavier <tomazcx06@gmail.com>
2024-08-06 10:02:47 -0300

    fix: font size

--- components/section/plans/item.vue
+++ components/section/plans/item.vue
@@ -6,7 +6,7 @@ defineProps<{
 </script>
 
 <template>
-  <li class="grid grid-cols-12 items-center gap-1.5 text-xs lg:text-lg font-bold">
+  <li class="grid grid-cols-12 items-center gap-1.5 text-sm lg:text-lg font-bold">
     <PhosphorIconCheck class="text-accent col-span-1" :size="24" v-if="check" />
     <PhosphorIconX :size="20" v-else />
     <span :class="{ 'line-through text-textSecondary': !check, 'col-span-11':true }">{{ text }}</span>
