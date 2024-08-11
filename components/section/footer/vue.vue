c622085f385c132062cf664f674312677e727aa4
Erick Scur <erickscur@gmail.com>
2024-08-10 21:13:58 -0300

    feat: add instagram link

--- components/section/footer/vue.vue
+++ components/section/footer/vue.vue
@@ -1,7 +1,9 @@
 <template>
   <footer class="flex items-center justify-center pt-8 font-bold">
     <p class="flex items-center gap-4">
-      <PhosphorIconInstagramLogo :size="28" /> <span>@umporcentoprog</span>
+      <a href="https://www.instagram.com/umporcentoprog" target="_blank" rel="noopener noreferrer" class="flex items-center gap-4">
+        <PhosphorIconInstagramLogo :size="28" /> <span>@umporcentoprog</span>
+      </a>
     </p>
   </footer>
 </template>
